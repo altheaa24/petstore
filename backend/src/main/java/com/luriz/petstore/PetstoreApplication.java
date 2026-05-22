ca887fc7516c331431859998b4c203e38b58c6d2
altheaa24 <aeluriz.student@ua.edu.ph>
2026-05-22 20:13:42 +0800

    Configured Global CORS settings to allow production frontend origin

--- backend/src/main/java/com/luriz/petstore/PetstoreApplication.java
+++ backend/src/main/java/com/luriz/petstore/PetstoreApplication.java
@@ -1,11 +1,30 @@
 package com.luriz.petstore;
 
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PetstoreApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(PetstoreApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://petstore-frontend-ztp4.onrender.com") // Your live frontend URL!
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
