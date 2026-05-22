13d38097c1091a2b8a52ad054ad4bb6d24cd951a
altheaa24 <aeluriz.student@ua.edu.ph>
2026-05-23 00:55:35 +0800

    Updated frontend api service to target active backend URL instance

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "https://petstore-backend-y14d.onrender.com/api";
+const API_BASE_URL = "https://petstore-backend-iceb.onrender.com/api";
 
 
 const api = axios.create({
